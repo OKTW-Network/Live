847dcd7ab4feb382dbcf5747605c8ca4368e5e3c
Mmis1000 <mmis1000@yahoo.com.tw>
2019-05-15 00:38:19 +0800

    Add popup viewer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <v-layout row wrap>
       <v-flex xs12 sm6 lg4 v-for="item in items" :key="item.format.filename">
         <v-card
-          :href="getLink(item)"
+          @click="play(item)"
         >
           <v-img
             :src="`${base}${item.format.filename.replace(/^.+\/(.+).flv$/, '$1')}.png`"
@@ -22,6 +22,17 @@
         </v-card>
       </v-flex>
     </v-layout>
+    <v-dialog v-model="dialog" :content-class="isMobile ? 'dynamic-wrap mobile' : 'dynamic-wrap'" transition="dialog-bottom-transition">
+      <v-card style="display: inline-block" v-if="dialog">
+        <a :href="videoSrc" class="open-in-new-window" :class="{mobile: isMobile}">
+          <v-icon large style="vertical-align: middle">open_in_new</v-icon>
+        </a>
+        <div @click="dialog = false" v-if="isMobile" class="close">
+          <v-icon large style="vertical-align: middle">close</v-icon>
+        </div>
+        <video controls :src="videoSrc" class="my-video" :class="{mobile: isMobile}"></video>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -34,7 +45,9 @@ export default {
   name: 'home',
   data () {
     return {
-      base: 'https://live.oktw.one/record/'
+      dialog: false,
+      base: 'https://live.oktw.one/record/',
+      videoSrc: ''
     }
   },
   components: {
@@ -57,6 +70,17 @@ export default {
       } else {
         return items
       }
+    },
+    isMobile () {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs':
+          return true
+        case 'sm': 
+        case 'md': 
+        case 'lg': 
+        case 'xl': 
+          return this.$vuetify.breakpoint.height < 450
+      }
     }
   },
   methods: {
@@ -77,7 +101,52 @@ export default {
       const hour = ~~(totalSec / 3600)
 
       return `${hour}:${min}:${sec}`
+    },
+    play(item) {
+      const url = this.getLink(item)
+      this.videoSrc = url
+      this.dialog = true
     }
   }
 }
 </script>
+<style>
+.dynamic-wrap {
+  width: auto;
+}
+.dynamic-wrap.mobile {
+  max-height: none;
+  margin: 0;
+}
+</style>
+<style scoped>
+.open-in-new-window, .close {
+  position: absolute;
+  display: block;
+  right: 0;
+  top: 0;
+  width: 48px;
+  height: 48px;
+  line-height: 48px;
+  text-align: center;
+  background: rgba(32, 32, 32, 0.2);
+  cursor: pointer;
+}
+
+.open-in-new-window.mobile {
+  right: 48px;
+}
+
+.my-video {
+  max-height: 90vh;
+  max-width:90vw;
+  vertical-align: middle;
+}
+.my-video.mobile {
+  max-height: 100vh;
+  height: 100vh;
+  max-width:100vw;
+  width: 100vw;
+  vertical-align: middle;
+}
+</style>
