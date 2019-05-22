742d5954c957866d2baa01d65b34167206876689
Sciuridae0603 <Sciuridae0603@gmail.com>
2019-05-22 22:11:44 +0800

    Fix 18 img

--- src/components/EighteenPlusWarning.vue
+++ src/components/EighteenPlusWarning.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-bind:style="EighteenPlusWarningClassObject" id="EighteenPlusWarning">
-    <div id="EighteenPlusWarningLogo"></div>
+    <img src="img/18.svg" id="EighteenPlusWarningLogo"></img>
     <button v-bind:style="AcceptButtonClassObject" class="EighteenPlusWarningButton" id="AcceptButton" @click="Accept">接受 Accept</button>
     <button class="EighteenPlusWarningButton" id="DeclinedButton" @click="Declined">拒絕 Declined</button>
   </div>
