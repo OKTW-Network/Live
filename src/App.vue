026f4a0566d05070ddab60d86fcd58e2cf887fff
Sciuridae0603 <Sciuridae0603@gmail.com>
2019-05-29 23:08:48 +0800

    Fix on live click change src title

--- src/App.vue
+++ src/App.vue
@@ -82,8 +82,8 @@ export default {
             }
           }else if(type === "streamerLive"){
             this.data.nowPlayer = "live";
-            this.data.recordPlayer.title = eventData.streamer.name;
-            this.data.recordPlayer.src = (eventData.streamer.unloadRecords.length === 0) ? `/live/live.m3u8` : `/live/${eventData.streamer.name}.m3u8`;
+            this.data.livePlayer.title = eventData.streamer.name;
+            this.data.livePlayer.src = (eventData.streamer.unloadRecords.length === 0) ? `/live/live.m3u8` : `/live/${eventData.streamer.name}.m3u8`;
             window.scrollTo(0,0);
           }
       }
