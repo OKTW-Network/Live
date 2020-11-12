54ae64ba18981c5092edc7c873148a94ee07f1f1
james58899 <james59988@gmail.com>
2020-11-12 14:31:26 +0000

    try

--- src/components/RecordList.vue
+++ src/components/RecordList.vue
@@ -1,5 +1,5 @@
 <template>
-  <perfect-scrollbar class="RecordListDiv" ref="viewBox" :options="{wheelPropagation: false, scrollXMarginOffset: '10px'}">
+  <perfect-scrollbar class="RecordListDiv" ref="viewBox" :options="{wheelPropagation: false, scrollXMarginOffset: 10}">
     <Record
       v-for="record in records"
       v-on:click="emitClick"
