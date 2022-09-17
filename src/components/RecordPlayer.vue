94b6dc161adad7965b94f2ae8976e68296993236
jimchen5209 <jimchen5209@gmail.com>
2022-09-17 22:00:56 +0800

    feat: replace record player with video.js

--- src/components/RecordPlayer.vue
+++ src/components/RecordPlayer.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="RecordPlayerDiv">
-    <video controls class="RecordPlayer" v-bind:src="video.src"/>
+    <video controls autoplay ref="RecordPlayer" class="RecordPlayer video-js vjs-default-skin vjs-big-play-centered" v-bind:data-poster="video.poster">
+      <source src="https://live.oktw.one/record/james58899-1662217396.mp4" type='video/mp4'/>
+      <!-- <source v-bind:src="video.src" type='video/mp4'/> -->
+    </video>
     <h1>{{video.title}}</h1>
     <h3>{{video.subtitle}}</h3>
   </div>
 </template>
 
 <script>
+import "video.js/dist/video-js.css";
+import videojs from 'video.js';
 
 export default {
   name: "RecordPlayer",
   props: {
     video: Object
+  },
+  data() {
+    return {
+      player: null
+    }
+  },
+  mounted() {
+    this.player = videojs(this.$refs.RecordPlayer, { fluid: true });
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
   }
 };
 </script>
@@ -31,5 +49,6 @@ export default {
   box-shadow: 0 0 0 3px #111;
   border-radius: 10px;
   box-sizing: border-box;
+  overflow: hidden;
 }
 </style>
